acd15009ce5e2b8940468d84a7201992dae0929b
Nomi Magnus <nomimagnus@gmail.com>
2026-06-16 23:53:05 +0300

    #5

--- app/db.py
+++ app/db.py
@@ -3,6 +3,9 @@ from typing import Generator, Optional
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/intelivox")
 
