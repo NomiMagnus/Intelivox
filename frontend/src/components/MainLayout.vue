1dd3aa52dd1c0346c3da87ef64fc53fd059c9ef5
Nomi Magnus <nomimagnus@gmail.com>
2026-06-24 00:03:12 +0300

    Fix alignment

--- frontend/src/components/MainLayout.vue
+++ frontend/src/components/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="neo-bg min-h-screen text-white">
     <div class="mx-auto flex min-h-screen max-w-[1600px] flex-col-reverse gap-4 p-4 lg:flex-row lg:gap-6 lg:p-6">
-      <main class="neo-panel flex-1 p-4 lg:p-6">
-        <router-view />
-      </main>
       <aside class="neo-panel w-full shrink-0 p-3 lg:w-80 lg:p-4">
         <Sidebar />
       </aside>
+      <main class="neo-panel flex-1 p-4 lg:p-6">
+        <router-view />
+      </main>
     </div>
   </div>
 </template>
